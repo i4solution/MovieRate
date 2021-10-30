fcd262335749c0a20485b06414dd1b9ab1af6e34
Vu Nguyen <vunguyen@Vus-MacBook-Pro.local>
2021-10-30 00:53:19 -0700

    Initialize

---
+++ Movie Rate/Movie Rate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Rate
+//
+//  Created by Vu Nguyen on 10/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
